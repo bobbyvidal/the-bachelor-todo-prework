a7c5f182d50ec9ad09749061457754701d88b856
Robert Vidal <bobbyvidal@gmail.com>
2018-12-15 21:43:22 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -9,16 +9,16 @@ def get_first_name_of_season_winner(data, season)
     end
 end	
 
-def get_first_name_of_season_winner(data, season)
-  data[season].each do |people|
-       people.each do |k,v|
-        if v == "Winner"
-            full_name = people["name"]
-            return full_name.split(' ').first
-        end 
+def get_contestant_name(data, occupation)
+   data.each do |season, array|
+    array.each do |people|
+      people.each do |k,v|
+        if v == occupation
+          return people["name"]
+        end
       end
     end
-end	
+  end
 
 def count_contestants_by_hometown(data, hometown)
   # code here
