98ed6dcced436ec3fde0622b2efe9d2e47dc2f6c
Robert Vidal <bobbyvidal@gmail.com>
2018-12-15 21:46:36 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -36,6 +36,20 @@ counter = 0
   end
 counter
  end 
+ 
+ 
+ def get_occupation(data, hometown)
+
+  data.each do |season, array|
+    array.each do |hash|
+      hash.each do |k,v|
+        if v == hometown
+          return hash["occupation"]
+        end
+      end
+    end
+  end
+ end	
 
 def get_average_age_for_season(data, season)
   # code here
