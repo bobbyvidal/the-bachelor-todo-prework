818942daa483e824c0390419f57582270badc041
Robert Vidal <bobbyvidal@gmail.com>
2018-12-15 21:44:28 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -25,9 +25,20 @@ def count_contestants_by_hometown(data, hometown)
   # code here
 end
 
-def get_occupation(data, hometown)
-  # code here
-end
+def count_contestants_by_hometown(data, hometown)
+  
+counter = 0
+  data.each do |season, array|
+    array.each do |hash|
+      hash.each do |k,v|
+        if v == hometown
+          counter += 1
+        end
+      end
+    end
+  end
+counter
+ end 
 
 def get_average_age_for_season(data, season)
   # code here
