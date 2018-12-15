9da118c59b16b67f1e9c14811fb3b15db17d308b
Robert Vidal <bobbyvidal@gmail.com>
2018-12-15 21:46:02 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -21,9 +21,6 @@ def get_contestant_name(data, occupation)
   end
 end
 
-def count_contestants_by_hometown(data, hometown)
-  # code here
-end
 
 def count_contestants_by_hometown(data, hometown)
   
